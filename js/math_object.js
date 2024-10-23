00e569f365de3112ab76c0ef4b545efaceafb538
karlagarcia1 <2302087@utrivieramaya.edu.mx>
2024-10-23 09:20:02 -0500

    DATATYPES

---
+++ js/math_object.js
@@ -0,0 +1,37 @@
+const PI = Math.PI
+console.log(PI); //3.1416
+console.log(Math.round(9.85)) //devuelve el valor de un numero redondeando al entero mas cercano
+console.log(Math.floor(PI)) // devuelve el mayor entero menor que o igual a un num
+console.log(Math.ceil(PI)) // devuelve el numero al entero mas pequeño mayor o igual a un numero
+console.log(Math.min(-5, -3, 20, 50, 5, 10, 0 )) //devuelve el valor mas pequeño de 0 o mas numeros
+console.log(Math.max(-5, -3, 20, 50, 5, 10, 0 )) // devuelve el valor mayor de 0 o mas numeros
+
+const radNum = Math.random()
+console.log(radNum) // devuelve un numero pseudo-aleatorio entre 0 y 1
+
+const num = Math.floor(math.random() * 11)
+console.log(num) //crea un numero aleatorio entre 0 y 10
+
+console.log(Math.abs(-10)) //devuelve el valor absoluto de un numero
+
+console.log(sqrt(100))
+console.log(Math.sqrt(2))
+
+console.log(Math.pow(3,2)) //las devoluciones en base a la potencia de exponente
+
+console-log(Math.E) //represemta la base de logaritmos naturales o aprox 2.718
+
+
+//LOGARITMOS
+
+console.log(Math.log(2)) //devuelve el logaritmo natural de un numero
+console.log(Math.log(10))
+
+// TRIGONOMETRIA
+
+console.log(Math.sin(0)) //devuelve el seno de un numero
+console.log(Math.sin(60))
+
+console.log(Math.cos(0)) //devuelve el coseno de un numero
+console.log(Math.cos(60))
+
---
+++ js/string_concatenation.js
@@ -0,0 +1,7 @@
+let name = 'karla';
+let lastName = 'garcia';
+let age = 19;
+let space = ' '
+
+let fullName = name + space + lastName + space + age;
+console.log(fullName)
